73111c706ceedbd1142f850372f766d82142f454
Ameliah Utary <ameliah1244@gmail.com>
2025-07-15 01:38:13 +0800

    delete

--- app.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-st.title("🧠 Machine Learning App")
-st.write("Selamat datang! Silakan pilih fitur dari sidebar 👉")
-
-st.markdown("### Menu:")
-st.markdown("- 🔍 Preprocessing Teks")
-st.markdown("- 🤖 Naive Bayes Sentiment Classification")
