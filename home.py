21a77bb67af1538e58cfb83f930309cc0f37a645
Ameliah Utary <ameliah1244@gmail.com>
2025-07-15 18:37:18 +0800

    Update home.py

--- home.py
+++ home.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(page_title="ENHYPEN ML App", layout="wide")
 
-st.title("✨ ENHYPEN Sentiment App")
+st.title("✨ Analysis Sentiment App")
 st.markdown("Selamat datang di aplikasi Machine Learning untuk analisis sentimen dan klasifikasi tweet ENHYPEN!")
 
 st.markdown("### 📂 Pilih halaman dari sidebar untuk mulai:")
