89b68cd7cc71a0baad3ef013331b448035cbd579
Ameliah Utary <ameliah1244@gmail.com>
2025-07-15 17:18:46 +0800

    Update home.py

--- home.py
+++ home.py
@@ -6,5 +6,6 @@ st.title("✨ ENHYPEN Sentiment App")
 st.markdown("Selamat datang di aplikasi Machine Learning untuk analisis sentimen dan klasifikasi tweet ENHYPEN!")
 
 st.markdown("### 📂 Pilih halaman dari sidebar untuk mulai:")
+st.markdown("- **Translate Data**")
 st.markdown("- **Preprocessing & Sentiment Labelling**")
 st.markdown("- **Naive Bayes Classification**")
